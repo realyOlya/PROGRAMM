3f196f6094df9781338da40692eab0c38a72a55b
Ksandra2605 <sewerolya@gmail.com>
2024-07-22 14:27:00 +0300

    3

--- 1.1.py
+++ 1.1.py
@@ -9,9 +9,15 @@ beauty = []
 smart =[]
 for ball in a:
     if ball > sr and ball > s:
-        if ball > 50 and ball < 200:
+        if ball > 190 and ball < 250:
             beauty.append(ball)
         if ball >= 250:
             smart.append(ball)
-print(len(beauty))
-print(len(smart))
+k_beauty = len(beauty)
+k_smart = round(k_beauty * 0.8) # 1023.2 - 80 %, максимально возможное получается 1023
+smart.sort(reverse=True)
+proxod_smart = smart[k_smart - 1]
+print(k_beauty, proxod_smart)
+
+
+#1279 268
