24681063c3e8f31a26566451f74ca30b113f92b5
Ksandra2605 <sewerolya@gmail.com>
2024-07-22 14:07:14 +0300

    3

--- 1.1.py
+++ 1.1.py
@@ -5,19 +5,13 @@ for line in f.readlines():
     ID, EGE, UD = [int(i) for i in line.split()]
     a.append(EGE + UD)
 sr = sum(a) / len(a)
-k = 0
-mx = None
-spisok = {}
+beauty = []
+smart =[]
 for ball in a:
     if ball > sr and ball > s:
-        k += 1
-    if ball not in spisok:
-        spisok[ball] = 0
-    spisok[ball] += 1
-for line in spisok:
-    if mx is None or spisok[mx] < spisok[line]:
-        mx = line
-print(k, mx)
-
-
-#2653 266
+        if ball > 50 and ball < 200:
+            beauty.append(ball)
+        if ball >= 250:
+            smart.append(ball)
+print(len(beauty))
+print(len(smart))
